10807d8d72387d32b788b116bbabbbfe3f52a494
kizmt <kizmetsol@proton.me>
2024-06-10 06:59:38 +1200

    remove-tx-params

--- app/tx/[signature]/page-client.tsx
+++ app/tx/[signature]/page-client.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { Address } from '@components/common/Address';
-import { BalanceDelta } from '@components/common/BalanceDelta';
 import { ErrorCard } from '@components/common/ErrorCard';
 import { InfoTooltip } from '@components/common/InfoTooltip';
 import { LoadingCard } from '@components/common/LoadingCard';
@@ -10,9 +8,6 @@ import { Slot } from '@components/common/Slot';
 import { SolBalance } from '@components/common/SolBalance';
 import { TableCardBody } from '@components/common/TableCardBody';
 import { SignatureContext } from '@components/instruction/SignatureContext';
-import { InstructionsSection } from '@components/transaction/InstructionsSection';
-import { ProgramLogSection } from '@components/transaction/ProgramLogSection';
-import { TokenBalancesCard } from '@components/transaction/TokenBalancesCard';
 import { FetchStatus } from '@providers/cache';
 import { useCluster } from '@providers/cluster';
 import {
@@ -22,14 +17,12 @@ import {
     useTransactionStatus,
 } from '@providers/transactions';
 import { useFetchTransactionDetails } from '@providers/transactions/parsed';
-import { ParsedTransaction, SystemInstruction, SystemProgram, TransactionSignature } from '@solana/web3.js';
+import { ParsedTransaction, TransactionSignature } from '@solana/web3.js';
 import { Cluster, ClusterStatus } from '@utils/cluster';
 import { displayTimestamp } from '@utils/date';
 import { SignatureProps } from '@utils/index';
 import { getTransactionInstructionError } from '@utils/program-err';
-import { intoTransactionInstruction } from '@utils/tx';
 import { useClusterPath } from '@utils/url';
-import { BigNumber } from 'bignumber.js';
 import bs58 from 'bs58';
 import Link from 'next/link';
 import React, { Suspense, useEffect, useState } from 'react';
@@ -183,22 +176,7 @@ function StatusCard({ signature, autoRefresh }: SignatureProps & AutoRefreshProp
 
     const transactionWithMeta = details?.data?.transactionWithMeta;
     const fee = transactionWithMeta?.meta?.fee;
-    const computeUnitsConsumed = transactionWithMeta?.meta?.computeUnitsConsumed;
     const transaction = transactionWithMeta?.transaction;
-    const blockhash = transaction?.message.recentBlockhash;
-    const version = transactionWithMeta?.version;
-    const isNonce = (() => {
-        if (!transaction || transaction.message.instructions.length < 1) {
-            return false;
-        }
-
-        const ix = intoTransactionInstruction(transaction, transaction.message.instructions[0]);
-        return (
-            ix &&
-            SystemProgram.programId.equals(ix.programId) &&
-            SystemInstruction.decodeInstructionType(ix) === 'AdvanceNonceAccount'
-        );
-    })();
 
     let statusClass = 'success';
     let statusText = 'Success';
@@ -292,11 +270,6 @@ function StatusCard({ signature, autoRefresh }: SignatureProps & AutoRefreshProp
                 </tr>
 
                 <tr>
-                    <td>Confirmations</td>
-                    <td className="text-lg-end text-uppercase">{info.confirmations}</td>
-                </tr>
-
-                <tr>
                     <td>Slot</td>
                     <td className="text-lg-end">
                         <Slot slot={info.slot} link />
@@ -312,12 +285,6 @@ function StatusCard({ signature, autoRefresh }: SignatureProps & AutoRefreshProp
                     </tr>
                 )}
 
-                {version !== undefined && (
-                    <tr>
-                        <td>Transaction Version</td>
-                        <td className="text-lg-end text-uppercase">{version}</td>
-                    </tr>
-                )}
             </TableCardBody>
         </div>
     );
@@ -356,74 +323,4 @@ function DetailsSection({ signature }: SignatureProps) {
     );
 }
 
-function AccountsCard({ signature }: SignatureProps) {
-    const details = useTransactionDetails(signature);
 
-    const transactionWithMeta = details?.data?.transactionWithMeta;
-    if (!transactionWithMeta) {
-        return null;
-    }
-
-    const { meta, transaction } = transactionWithMeta;
-    const { message } = transaction;
-
-    if (!meta) {
-        return <ErrorCard text="Transaction metadata is missing" />;
-    }
-
-    const accountRows = message.accountKeys.map((account, index) => {
-        const pre = meta.preBalances[index];
-        const post = meta.postBalances[index];
-        const pubkey = account.pubkey;
-        const key = account.pubkey.toBase58();
-        const delta = new BigNumber(post).minus(new BigNumber(pre));
-
-        return (
-            <tr key={key}>
-                <td>{index + 1}</td>
-                <td>
-                    <Address pubkey={pubkey} link fetchTokenLabelInfo />
-                </td>
-                <td>
-                    <BalanceDelta delta={delta} isSol />
-                </td>
-                <td>
-                    <SolBalance lamports={post} />
-                </td>
-                <td>
-                    {index === 0 && <span className="badge bg-info-soft me-1">Fee Payer</span>}
-                    {account.signer && <span className="badge bg-info-soft me-1">Signer</span>}
-                    {account.writable && <span className="badge bg-danger-soft me-1">Writable</span>}
-                    {message.instructions.find(ix => ix.programId.equals(pubkey)) && (
-                        <span className="badge bg-warning-soft me-1">Program</span>
-                    )}
-                    {account.source === 'lookupTable' && (
-                        <span className="badge bg-gray-soft me-1">Address Table Lookup</span>
-                    )}
-                </td>
-            </tr>
-        );
-    });
-
-    return (
-        <div className="card">
-            <div className="card-header">
-                <h3 className="card-header-title">Account Input(s)</h3>
-            </div>
-            <div className="table-responsive mb-0">
-                <table className="table table-sm table-nowrap card-table">
-                    <thead>
-                        <tr>
-                            <th className="text-muted">#</th>
-                            <th className="text-muted">Address</th>
-                            <th className="text-muted">Change (SOL)</th>
-                            <th className="text-muted">Post Balance (SOL)</th>
-                            <th className="text-muted">Details</th>
-                        </tr>
-                    </thead>
-                    <tbody className="list">{accountRows}</tbody>
-                </table>
-            </div>
-        </div>
-    );
-}
